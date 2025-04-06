ca16b7ce3e3632ab81b977c3831fc765951c55f2
MardisTech <mardistech@gmail.com>
2025-04-06 11:38:08 -0400

    add slow button

--- sol_sys.py
+++ sol_sys.py
@@ -39,9 +39,12 @@ frames = 0
 pause = False
 place_active = False
 
+
 TIMESTEP = (
         3600 * 24
     )  # the difference in time betweent frames. Since we want each frame to represent one day, we will use the amount of seconds in a day
+TIMESTEP_MULTIPLIER = 1
+
 
 class Planet:
     AU = 149.6e6 * 1000
@@ -50,7 +53,7 @@ class Planet:
 
     global TIMESTEP
 
-    def __init__(self, x, y, radius, color, mass, name):
+    def __init__(self, x, y, radius, color, mass, name, x_vel = 0, y_vel = 0):
         self.x = x
         self.y = y
         self.radius = radius
@@ -60,8 +63,8 @@ class Planet:
         self.TIMESTEP = TIMESTEP
         self.sun = False
         self.distance_to_sun = 0
-        self.x_vel = 0
-        self.y_vel = 0
+        self.x_vel = x_vel
+        self.y_vel = y_vel
         self.orbit = (
             []
         )  # this is a list of x,y position values for each frame for each planet so we can draw the planet and calculate distance from other planets through the pixel/distance ratio
@@ -75,8 +78,8 @@ class Planet:
         self.TIMESTEP0 = TIMESTEP
         self.sun0 = False
         self.distance_to_sun0 = 0
-        self.x_vel0 = 0 # need to add a initial velocity parameter for object
-        self.y_vel0 = 0
+        self.x_vel0 = x_vel
+        self.y_vel0 = y_vel
         self.orbit0 = (
             []
         )
@@ -178,55 +181,55 @@ def main():
     sun.sun = True
 
     earth = Planet(
-        -1 * Planet.AU, 0, 4, BLUE, 5.9742 * 10**24, "Earth"
+        -1 * Planet.AU, 0, 4, BLUE, 5.9742 * 10**24, "Earth", y_vel = 29.783 * 1000
     )  # (-1 * Planet.AU, 0, 16, BLUE, 5.9742 * 10**24)
     earth.y_vel = 29.783 * 1000
 
     mars = Planet(
-        -1.524 * Planet.AU, 0, 3, RED, 6.39 * 10**23, "Mars"
+        -1.524 * Planet.AU, 0, 3, RED, 6.39 * 10**23, "Mars", y_vel = 24.077 * 1000
     )  # (-1.524 * Planet.AU, 0, 12, RED, 6.39 * 10**23)
     mars.y_vel = 24.077 * 1000
 
     mercury = Planet(
-        0.387 * Planet.AU, 0, 2, DARK_GRAY, 3.30 * 10**23, "Mercury"
+        0.387 * Planet.AU, 0, 2, DARK_GRAY, 3.30 * 10**23, "Mercury", y_vel = -47.4 * 1000
     )  # 0.387 * Planet.AU, 0, 8, DARK_GRAY, 3.30 * 10**23
     mercury.y_vel = -47.4 * 1000
 
     venus = Planet(
-        0.723 * Planet.AU, 0, 3.5, WHITE, 4.8685 * 10**24, "Venus"
+        0.723 * Planet.AU, 0, 3.5, WHITE, 4.8685 * 10**24, "Venus", y_vel = -35.02 * 1000
     )  # 0.723 * Planet.AU, 0, 14, WHITE, 4.8685 * 10**24
     venus.y_vel = -35.02 * 1000
 
-    jupiter = Planet(-5.2 * Planet.AU, 0, 14, ORANGE, 1898.13 * 10**24, "Jupiter")
+    jupiter = Planet(-5.2 * Planet.AU, 0, 14, ORANGE, 1898.13 * 10**24, "Jupiter", y_vel = 13.07 * 1000)
     jupiter.y_vel = 13.07 * 1000
 
-    saturn = Planet(9.5 * Planet.AU, 0, 12, TAN, 5.683 * 10**26, "Saturn")
+    saturn = Planet(9.5 * Planet.AU, 0, 12, TAN, 5.683 * 10**26, "Saturn", y_vel = -9.68 * 1000)
     saturn.y_vel = -9.68 * 1000
 
-    uranus = Planet(-19.8 * Planet.AU, 0, 10, BABY_BLUE, 8.6 * 10**25, "Uranus")
+    uranus = Planet(-19.8 * Planet.AU, 0, 10, BABY_BLUE, 8.6 * 10**25, "Uranus", y_vel = 6.8 * 1000)
     uranus.y_vel = 6.8 * 1000
 
-    neptune = Planet(30 * Planet.AU, 0, 9, DARK_BLUE, 1.024 * 10**24, "Neptune")  #
+    neptune = Planet(30 * Planet.AU, 0, 9, DARK_BLUE, 1.024 * 10**24, "Neptune", y_vel = -5.43 * 1000)  #
     neptune.y_vel = -5.43 * 1000
 
     #   optional objects that user can choose to add to game by clicking the corresponding button
     black_hole = Planet(
-        15 * Planet.AU, 20 * Planet.AU, 2, WHITE, 5 * sun.mass, "Black Hole"
+        15 * Planet.AU, 20 * Planet.AU, 2, WHITE, 5 * sun.mass, "Black Hole", y_vel = -6 * 1000
     )
     black_hole.y_vel = -6 * 1000
 
     second_sun = Planet(
-        15 * Planet.AU, 20 * Planet.AU, 7.5, YELLOW, 1.98892 * 10**30, "Sun"
+        15 * Planet.AU, 20 * Planet.AU, 7.5, YELLOW, 1.98892 * 10**30, "Sun 2.0", y_vel = -6 * 1000
     )  # (0, 0, 30, YELLOW, 1.98892 * 10**30)
     second_sun.y_vel = -6 * 1000
 
     second_jupiter = Planet(
-        15 * Planet.AU, 20 * Planet.AU, 5, ORANGE, 1898.13 * 10**24, "Jupiter"
+        15 * Planet.AU, 20 * Planet.AU, 5, ORANGE, 1898.13 * 10**24, "Jupiter 2.0", y_vel = -6 * 1000
     )
     second_jupiter.y_vel = -6 * 1000
 
     user_object = Planet(
-        5 * Planet.AU, 5 * Planet.AU, 3, WHITE, 10 * sun.mass, "Custom"
+        5 * Planet.AU, 5 * Planet.AU, 3, WHITE, 10 * sun.mass, "BH"
     )
 
     #   main() iterates through this list of objects and updates each position. optional objects get entered here when clicked
@@ -431,9 +434,8 @@ def main():
         button_h,
         inactive_color,
         active_color,
-        add_planet=None,
     ):
-        global TIMESTEP
+        global TIMESTEP, TIMESTEP_MULTIPLIER
         mouse = pygame.mouse.get_pos()
         click = pygame.mouse.get_pressed()
 
@@ -448,7 +450,47 @@ def main():
 
             if click[0] == 1:
                 paused()
-                TIMESTEP = TIMESTEP * 2
+                TIMESTEP_MULTIPLIER = TIMESTEP_MULTIPLIER * 2
+                TIMESTEP = TIMESTEP*TIMESTEP_MULTIPLIER
+                for planet in planets:
+                    planet.update_timestep(new_timestep=TIMESTEP)
+
+        else:
+            pygame.draw.rect(
+                WIN, inactive_color, (button_x, button_y, button_w, button_h)
+            )
+
+        button_text = FONT.render(msg, True, BLACK)
+        textRect = button_text.get_rect()
+        textRect.center = ((button_x + (button_w / 2)), (button_y + (button_h / 2)))
+        WIN.blit(button_text, textRect)
+
+    def slow_button(
+        msg,
+        button_x,
+        button_y,
+        button_w,
+        button_h,
+        inactive_color,
+        active_color,
+    ):
+        global TIMESTEP, TIMESTEP_MULTIPLIER
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+
+        if (
+
+            button_x + button_w > mouse[0] > button_x
+            and button_y + button_h > mouse[1] > button_y
+        ):
+            pygame.draw.rect(
+                WIN, active_color, (button_x, button_y, button_w, button_h)
+            )
+
+            if click[0] == 1:
+                paused()
+                TIMESTEP_MULTIPLIER = 1/2
+                TIMESTEP = TIMESTEP*TIMESTEP_MULTIPLIER
                 for planet in planets:
                     planet.update_timestep(new_timestep=TIMESTEP)
 
@@ -491,7 +533,7 @@ def main():
 
     def paused():
         WIN.fill(BLACK)
-        global pause
+        global pause,TIMESTEP_MULTIPLIER
         pause = True
 
         while pause:
@@ -514,7 +556,7 @@ def main():
 
             for planet in planets:
                 planet.draw(WIN)
-            timeElapsedBox(frames)
+            timeElapsedBox(frames*TIMESTEP_MULTIPLIER)
             pygame.display.update()
             clock.tick(15)
 
@@ -619,7 +661,8 @@ def main():
 
         place_planet_button("Place BH", 25 * SCREEN_CHANGE_RATIO_W, 450 * SCREEN_CHANGE_RATIO_H, BUTTON_WIDTH, BUTTON_HEIGHT, WHITE, YELLOW)
 
-        speed_up_button(">>", 25 * SCREEN_CHANGE_RATIO_W, 670 * SCREEN_CHANGE_RATIO_H, BUTTON_WIDTH, BUTTON_HEIGHT, YELLOW, RED)
+        speed_up_button(">>>", 25 * SCREEN_CHANGE_RATIO_W, 610 * SCREEN_CHANGE_RATIO_H, BUTTON_WIDTH, BUTTON_HEIGHT, YELLOW, RED)
+        slow_button(">", 25 * SCREEN_CHANGE_RATIO_W, 670 * SCREEN_CHANGE_RATIO_H, BUTTON_WIDTH, BUTTON_HEIGHT, YELLOW, RED)
 
         pause_button("Pause", 25 * SCREEN_CHANGE_RATIO_W, 730 * SCREEN_CHANGE_RATIO_H, BUTTON_WIDTH, BUTTON_HEIGHT, YELLOW, RED)
         reset_button("Reset", 25 * SCREEN_CHANGE_RATIO_W, 790 * SCREEN_CHANGE_RATIO_H, BUTTON_WIDTH, BUTTON_HEIGHT, RED, RED)
@@ -630,7 +673,7 @@ def main():
 
         global frames, place_active
         frames += 1
-        timeElapsedBox(frames)
+        timeElapsedBox(frames*TIMESTEP_MULTIPLIER)
 
         # event listeners
         for event in pygame.event.get():
